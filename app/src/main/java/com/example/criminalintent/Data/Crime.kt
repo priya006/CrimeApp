eea5bf1eb5fc448969f66abf7107cfa7787e84e6
Priya Boopathi <priyanandh06@gmail.com>
2024-03-27 12:02:36 -0700

    Added layout file and string resource file

---
+++ app/src/main/java/com/example/criminalintent/Data/Crime.kt
@@ -0,0 +1,12 @@
+package com.example.criminalintent.Data
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    //The crimeID serves as a unique identifier for each crime in the list view
+    val crimeID: UUID,
+    val title : String,
+    val date: Date,
+    val isSolved: Boolean
+)
