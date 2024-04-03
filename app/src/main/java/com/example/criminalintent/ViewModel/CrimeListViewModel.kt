7ff080c6f2d8624e5f55e2b0259dacaa237c28bf
Priya Boopathi <priyanandh06@gmail.com>
2024-04-02 19:22:39 -0700

    Tweaked the constraints to make the UI look better

--- app/src/main/java/com/example/criminalintent/ViewModel/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/ViewModel/CrimeListViewModel.kt
@@ -8,10 +8,10 @@ class CrimeListViewModel : ViewModel() {
 
     init {
         //Based on the data passed to requirespolice we determine which layout to render
-        for (i in 0 until 10 ){
-            crimeObjects.add(i,Crime("crime #$i", "Sink issue", "20-11-2024", false, false))
+        for (i in 0 until 25 ){
+            crimeObjects.add(i,Crime("crime #$i", "Sink issue", "2024-02-15T15:30:00-08:00", false, false))
         }
-        crimeObjects.add(9,Crime("crime #$9", "Sink issue", "20-11-2024", false, true))
+        crimeObjects.add(9,Crime("crime #$9", "Sink issue", "2023-02-15T15:30:00-08:00", false, true))
     }
 
     fun getCrimeObject(): MutableList<Crime> {
